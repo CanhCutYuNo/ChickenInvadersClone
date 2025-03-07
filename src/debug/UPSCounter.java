832f796bd5e0291bb0c61889e9ff6028f8901a35
hungtvlt <tuanhungvu0912@gmail.com>
2025-03-07 17:53:19 +0700

    add debug components

---
+++ src/debug/UPSCounter.java
@@ -0,0 +1,41 @@
+package debug;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hp
+ */
+
+
+/**
+ * How to use: 
+ * --> Call only 1 function UPSCounter.Update() to get UPS value every 1 second in STDOUT
+ * 
+ */
+public abstract class UPSCounter {
+    private static long lastTime;
+    private static int frames;
+    private static int fps;
+
+    public UPSCounter() {
+        lastTime = System.nanoTime();
+        frames = 0;
+        fps = 0;
+    }
+
+    public static void Update() {
+        frames++;
+        long currentTime = System.nanoTime();
+        if (currentTime - lastTime >= 1_000_000_000) { // 1 second
+            fps = frames;
+            frames = 0;
+            lastTime = currentTime;
+            System.out.println("UPS: " + fps);
+        }
+    }
+
+}
