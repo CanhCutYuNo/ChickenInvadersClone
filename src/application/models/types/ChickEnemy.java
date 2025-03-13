66046774bb166d855ea879afab4529df3f05312a
hungtvlt <tuanhungvu0912@gmail.com>
2025-03-13 10:00:39 +0700

    fix ChickEnemy model

--- src/application/models/types/ChickEnemy.java
+++ src/application/models/types/ChickEnemy.java
@@ -20,7 +20,7 @@ public abstract class ChickEnemy extends Enemy {
 
     public ChickEnemy(int PosX, int PosY) {
         super(50, 46, 54, PosX, PosY);
-
+        currentFrame = (int) (Math.random() % 26);
         frameCount = 0;
         spriteSheet = new ImageIcon(getClass().getResource("/asset/resources/gfx/chick.png")).getImage();
     }
@@ -30,14 +30,7 @@ public abstract class ChickEnemy extends Enemy {
         {1, 83, 78, 79}, {81, 83, 78, 79}, {161, 83, 80, 79}, {243, 83, 80, 78}, {325, 83, 82, 78}, {409, 83, 84, 78},
         {1, 165, 86, 77}, {89, 165, 86, 77}, {177, 165, 88, 77}, {267, 165, 90, 76}, {359, 165, 91, 76},
         {1, 245, 92, 76}, {95, 245, 92, 75}, {189, 245, 92, 75}, {283, 245, 92, 75}, {377, 245, 92, 75},
-        {1, 323, 92, 75}, {95, 323, 92, 75}, {189, 323, 92, 75}, {283, 323, 92, 75}, // reverse
-    //        {189, 323, 92, 75}, {95, 323, 92, 75}, {1, 323, 92, 75},
-    //        {377, 245, 92, 75}, {283, 245, 92, 75}, {189, 245, 92, 75}, {95, 245, 92, 75},
-    //        {1, 245, 92, 76}, {359, 165, 91, 76}, {267, 165, 90, 76}, {177, 165, 88, 77},
-    //        {89, 165, 86, 77}, {1, 165, 86, 77}, {409, 83, 84, 78}, {325, 83, 82, 78},
-    //        {243, 83, 80, 78}, {161, 83, 80, 79}, {81, 83, 78, 79}, {1, 83, 78, 79},
-    //        {391, 1, 78, 79}, {313, 1, 76, 79}, {235, 1, 76, 80}, {157, 1, 76, 80},
-    //        {79, 1, 76, 80}, {1, 1, 76, 80}
+        {1, 323, 92, 75}, {95, 323, 92, 75}, {189, 323, 92, 75}, {283, 323, 92, 75}
     };
 
     private static final int[] offsetX = {
@@ -46,16 +39,16 @@ public abstract class ChickEnemy extends Enemy {
 
     @Override
     public void render(Graphics g) {
-        if (frameCount >= SPRITE.length) {
-            frameCount = 0;
-        }
+//        if (frameCount >= SPRITE.length) {
+//            frameCount = 0;
+//        }
         g.drawImage(spriteSheet,
-                PosX - offsetX[frameCount] - 15, PosY - 15, PosX + SPRITE[frameCount][2] - offsetX[frameCount] - 15, PosY + SPRITE[frameCount][3] - 15,
-                SPRITE[frameCount][0], SPRITE[frameCount][1], SPRITE[frameCount][0] + SPRITE[frameCount][2], SPRITE[frameCount][1] + SPRITE[frameCount][3], null);
+                PosX - offsetX[currentFrame] - 15, PosY - 15, PosX + SPRITE[currentFrame][2] - offsetX[currentFrame] - 15, PosY + SPRITE[currentFrame][3] - 15,
+                SPRITE[currentFrame][0], SPRITE[currentFrame][1], SPRITE[currentFrame][0] + SPRITE[currentFrame][2], SPRITE[currentFrame][1] + SPRITE[currentFrame][3], null);
 
         //debug
-        g.setColor(Color.WHITE);
-        g.drawRect(PosX, PosY,MODEL_WIDTH, MODEL_HEIGHT);
+//        g.setColor(Color.WHITE);
+//        g.drawRect(PosX, PosY,MODEL_WIDTH, MODEL_HEIGHT);
         
         //Vẽ chấm đỏ ở trung tâm
 //        int centerX = PosX + SPRITE[frameCount][2] / 2;
@@ -65,7 +58,22 @@ public abstract class ChickEnemy extends Enemy {
 
 
 //        System.out.println("x = " + centerX + ", y = " + centerY + ", f = " + frameCount);
-        frameCount++;
+//        frameCount++;
+    }
+    
+    @Override
+    public void nextFrame() {
+        if (isForward) {
+            currentFrame++;
+            if (currentFrame >= 25) {
+                isForward = false; // Đổi hướng khi đến cuối mảng
+            }
+        } else {
+            currentFrame--;
+            if (currentFrame <= 0) {
+                isForward = true; // Đổi hướng khi về đầu mảng
+            }
+        }
     }
 
     @Override
