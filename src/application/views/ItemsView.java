33998ee572a5b8f8fbbb88cc1c1fe12452d7bb34
bangncph25477 <113442198+bangnguyen75@users.noreply.github.com>
2025-04-11 03:39:37 +0700

    add sound

--- src/application/views/ItemsView.java
+++ src/application/views/ItemsView.java
@@ -23,11 +23,11 @@ public class ItemsView {
         g.drawImage(I, item.getPosX(), item.getPosY(), 40, 60, null);
 
         // Vẽ hitbox (chuyển sang Graphics2D để vẽ dễ hơn)
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.RED); // Màu hitbox
-
-        Rectangle hitbox = item.getHitbox();
-        g2d.draw(hitbox);
+//        Graphics2D g2d = (Graphics2D) g;
+//        g2d.setColor(Color.RED); // Màu hitbox
+//
+//        Rectangle hitbox = item.getHitbox();
+//        g2d.draw(hitbox);
     }
 
 }
